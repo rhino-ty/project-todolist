33861af932ac9c7dbbf07f6413c89c64d46de01e
rhino-ty <okko8522@gmail.com>
2022-12-12 18:14:45 +0900

    feat: Daily 페이지 구현

--- src/pages/Daily.js
+++ src/pages/Daily.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Lists from "../components/Lists";
+import useFetch from "../util/useFetch";
+import Loading from "../components/Loading";
+import { listDelete, listUpdate, doneUpdate } from "../util/todoApi";
 
 const Daily = () => {
-  return <div></div>;
+  const url = "http://localhost:3001/dailyTodos";
+  const { list, loading } = useFetch(url);
+
+  const handleItemToggle = (id, done) => {
+    console.log(id);
+    doneUpdate(url, id, done);
+  };
+
+  const handleItemRevise = (id, data) => {
+    console.log(id);
+    listUpdate(url, id, data);
+  };
+
+  const handleItemRemove = (id) => {
+    console.log(id);
+    listDelete(url, id);
+  };
+
+  // side effect 때문에 listData가 구성될때 한번 리렌더링되게 하기!
+  useEffect(() => {}, [list]);
+
+  return (
+    <>
+      {!loading && list ? (
+        <Lists
+          listData={list}
+          handleItemToggle={handleItemToggle}
+          handleItemRevise={handleItemRevise}
+          handleItemRemove={handleItemRemove}
+        />
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 };
 
 export default Daily;
